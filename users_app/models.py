8e1fbb4651bd08cf9fb580477e4e4e7e16131498
mcstao <arislg99@gmail.com>
2023-12-19 19:39:40 +0600

    Created models for users app

--- users_app/models.py
+++ users_app/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    username = models.CharField(unique=True, max_length=30)
+    email = models.EmailField(unique=True, max_length=30)
+    is_verified = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'username'
+
+    def __str__(self):
+        return self.username
+
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birth_date = models.DateField()
+    phone_regex = RegexValidator(
+        regex=r'^\+996\d{9}$',
+        message="Номер должен быть такого формата: '+996xxxxxxxxx'."
+    )
+    phone_number = models.CharField(validators=[phone_regex], max_length=13, unique=True, blank=True)
+    profile_image = models.ImageField()
+
+
