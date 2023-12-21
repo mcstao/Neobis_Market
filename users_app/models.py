72f6a2d41ed2432e630fed818f84fe5a1e7592b9
mcstao <arislg99@gmail.com>
2023-12-21 23:43:57 +0600

    Update avatar

--- users_app/models.py
+++ users_app/models.py
@@ -15,7 +15,7 @@ class CustomUser(AbstractUser):
         message="Номер должен быть такого формата: '0xxxxxxxxx'."
     )
     phone_number = models.CharField(validators=[phone_regex], max_length=10, unique=True, blank=True, null=True)
-    avatar = models.ImageField(upload_to='avatar_pics/', default='default/default.jpg', null=True, blank=True)
+    avatar = models.ImageField(max_length=300, upload_to='avatar_pics/', default='default/default.jpg', null=True, blank=True)
     verify_code = models.CharField(max_length=4, blank=True, null=True)
     is_verified = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
